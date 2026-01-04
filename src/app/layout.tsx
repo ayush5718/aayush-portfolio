8b69f7d1d4a850a0cdb471bbb6d7bc03dc50432e
Aayush Kumar <ayushbhardwaj9504@gmail.com>
2026-01-04 22:25:09 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,16 @@ export const metadata: Metadata = {
     google: "",
     yandex: "",
   },
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
